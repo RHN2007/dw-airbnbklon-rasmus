821803e44d929a6bc3e1e12cc6439af7c1bf3f6d
Rasmus Nielsen <rasmushulbaknielsen@gmail.com>
2026-09-14 11:56:03 +0200

    rettet fetchdata functionen i javascript til at kunne håndtere både index.html OG destination.html + tilføjet destination.html

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,31 @@
 const apartmentsList = document.querySelector(".apartments__list")
-
-
-// for (let i = 1; i <= 8; i++) {
-//     createListItem(i)
-// }
+const url = window.location.href
+const params = new URL(url).searchParams
+const destinationID = params.get("id")
 
 async function fetchData() {
-        try {
-        const response = await fetch("./data/destinations.json")
+    try {
+        let isProductPage = false
+        let response = ""
+
+        if (destinationID !== null) {
+            response = await fetch(`./data/${destinationID}.json`)
+            isProductPage = true
+        } else {
+            response = await fetch("./data/destinations.json")
+        }
+        
         let result = await response.json()
 
         if (!response.ok) {
             throw new Error("Fetch failed!")
         }
 
-        createListItem(result.destinations)
+        if (isProductPage == false) {
+            createListItem(result.destinations)
+        } else {
+            createDestinationPreview(result)
+        }
 
     } catch (error) {
         alert(error)
@@ -24,38 +35,35 @@ async function fetchData() {
 
 function createListItem(destinations) {
     destinations.forEach(destination => {
-            const listItem = document.createElement("li")
-            listItem.classList.add("list__item")
+        const listItem = document.createElement("li")
+        listItem.classList.add("list__item")
 
-            const itemFigure = document.createElement("figure")
-            itemFigure.classList.add("list__item__figure")
+        const itemFigure = document.createElement("figure")
+        itemFigure.classList.add("list__item__figure")
 
-            const figureImage = document.createElement("img")
-            figureImage.setAttribute("src", `img/${destination.image}`)
-            itemFigure.append(figureImage)
+        const figureImage = document.createElement("img")
+        figureImage.setAttribute("src", `img/${destination.image}`)
+        itemFigure.append(figureImage)
 
-            const itemDiv = document.createElement("div")
-            itemDiv.classList.add("list__item__div")
+        const itemDiv = document.createElement("div")
+        itemDiv.classList.add("list__item__div")
 
-            const itemFavourite = document.createElement("img") // skal ændres til button da vi skal kunne favourite forskellige destinations, men det er en ekstra opgave...
-            itemFavourite.setAttribute("src", "img/icons/notfavourite.svg")
+        const itemFavourite = document.createElement("img") // skal ændres til button da vi skal kunne favourite forskellige destinations, men det er en ekstra opgave...
+        itemFavourite.setAttribute("src", "img/icons/notfavourite.svg")
 
-            const itemLink = document.createElement("a")
-            itemLink.textContent = "More"
-            itemLink.setAttribute("href", `destination.html?id=${destination.id}`)
+        const itemLink = document.createElement("a")
+        itemLink.textContent = "More"
+        itemLink.setAttribute("href", `destination.html?id=${destination.id}`)
 
-            itemDiv.append(itemFavourite, itemLink)
+        itemDiv.append(itemFavourite, itemLink)
 
-            listItem.append(itemFigure, itemDiv)
-            apartmentsList.append(listItem)
-        })
+        listItem.append(itemFigure, itemDiv)
+        apartmentsList.append(listItem)
+    })
+}
+
+function createDestinationPreview (data) {
+    console.log(data)
 }
 
 fetchData()
-        // const listItem = document.createElement("li")
-        
-        // const itemImage = document.createElement("img")
-        // itemImage.setAttribute("src", `img/${result.image}`)
-
-        // listItem.append(itemImage)
-        // apartmentsList.append(listItem)
